c9c40521d20a73061ada85beb42c88450970bdb6
Joseph Manahan <jmanahan@mail.sfsu.edu>
2023-03-05 16:39:54 -0800

    Initial Commit

---
+++ ATMloop-Manahan/ATMloop_ManahanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATMloop_ManahanApp.swift
+//  ATMloop-Manahan
+//
+//  Created by Joseph Manahan on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ATMloop_ManahanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
